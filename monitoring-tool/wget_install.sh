b0ebc30cb1c30c161b3eca7eb0ed00f2f781bc6a
Ant Morales <100240691+hansha-nyc@users.noreply.github.com>
2026-09-23 21:18:00 -0700

    Create wget_install.sh for Prometheus setup
    Add wget installation script for Prometheus.

---
+++ monitoring-tool/wget_install.sh
@@ -0,0 +1,9 @@
+# wget based install for prometheus.
+# Antman - Milkyway
+
+#!/bin/bash
+
+wget https://prometheus.io/download/#:~:text=prometheus%2D3.14.0.linux%2Damd64.tar.gz
+
+tar xvfz prometheus-*.tar.gz
+cd prometheus-*
